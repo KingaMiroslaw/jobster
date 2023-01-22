d749e54a7c687a0601e762008c74e9ec97c66f76
KingaMiroslaw <106964401+KingaMiroslaw@users.noreply.github.com>
2023-01-22 20:39:14 +0100

    register user complete

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -12,9 +12,9 @@ export const registerUser = createAsyncThunk(
   async (user, thunkAPI) => {
     try {
       const resp = await customFetch.post("/auth/register", user);
-      console.log(resp)
+      return resp.data;
     } catch (error) {
-      console.log(error.response);
+      return thunkAPI.rejectWithValue(error.response.data.msg);
     }
   }
 );
@@ -29,6 +29,22 @@ export const loginUser = createAsyncThunk(
 const userSlice = createSlice({
   name: "user",
   initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(registerUser.fulfilled, (state, { payload }) => {
+        const { user } = payload;
+        state.isLoading = false;
+        state.user = user;
+        toast.success(`Hello There ${user.name}`);
+      })
+      .addCase(registerUser.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        toast.error(payload);
+      });
+  },
 });
 
 export default userSlice.reducer;
