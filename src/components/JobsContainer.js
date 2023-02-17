bb4b86cad76f43f619c49980a0d5dd8e7a41efb9
KingaMiroslaw <106964401+KingaMiroslaw@users.noreply.github.com>
2023-02-17 16:29:44 +0100

    JobsContainer, Loading setup

--- src/components/JobsContainer.js
+++ src/components/JobsContainer.js
@@ -1,5 +1,58 @@
+import { useEffect } from "react";
+import Job from "./Job";
+import styled from "styled-components";
+import { useSelector, useDispatch } from "react-redux";
+import Loading from "./Loading";
+
 function JobsContainer() {
-  return <h2>JobsContainer</h2>;
+  const { jobs, isLoading } = useSelector((store) => store.allJobs);
+  const dispatch = useDispatch();
+
+  if (isLoading) {
+    return <Loading center />;
+  }
+
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display...</h2>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper>
+      <h5>jobs info</h5>
+      <div className="jobs">
+        {jobs.map((job) => {
+          console.log(job);
+          return <Job key={job._id} {...job} />;
+        })}
+      </div>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.section`
+  margin-top: 4rem;
+  h2 {
+    text-transform: none;
+  }
+  & > h5 {
+    font-weight: 700;
+  }
+  .jobs {
+    display: grid;
+    grid-template-columns: 1fr;
+    row-gap: 2rem;
+  }
+  @media (min-width: 992px) {
+    .jobs {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      gap: 1rem;
+    }
+  }
+`;
 
 export default JobsContainer;
