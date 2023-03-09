f434907e4b32b24812b4bac35091ea04db2d2c0b
KingaMiroslaw <106964401+KingaMiroslaw@users.noreply.github.com>
2023-03-09 12:16:33 +0100

    create showStats request

--- src/pages/dashboard/Stats.js
+++ src/pages/dashboard/Stats.js
@@ -1,5 +1,15 @@
-function Stats() {
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { showStats } from "../../features/allJobs/allJobsSlice";
+
+const Stats = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(showStats());
+  }, [dispatch]);
+
   return <h1>Stats</h1>;
-}
+};
 
 export default Stats;
